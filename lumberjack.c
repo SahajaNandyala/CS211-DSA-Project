9ffdc3b52ac1bfd2423c4996dafcdc8edf0834c6
McQueen <63579517+mcqueen444@users.noreply.github.com>
2021-09-28 16:45:49 +0530

    Update lumberjack.c

--- lumberjack.c
+++ lumberjack.c
@@ -2,56 +2,51 @@
 #include<stdlib.h>
 #include "LinkedList.h"
 
-struct tree
-{
-    int x, y, h, d, c, p;
-    int value, weight, price;
-
-};
 
 int main()
 {
-    struct tree *tree_prop;
+    struct tree_prop *tree_prop;
 	int time_limit, grid_size,no_of_trees;
 	int i;
 
-    insert();
-    display();
     // taking time limit, grid size and number of trees as input
 	scanf("%d %d %d", &time_limit, &grid_size, &no_of_trees);
-
-    // array of trees
-	tree_prop = (struct tree*)malloc(no_of_trees*sizeof(struct tree));
 
     // taking input of each tree properties
     for(i=0;i<no_of_trees;i++)
 	{
-		scanf("%d %d %d %d %d %d",&tree_prop[i].x, &tree_prop[i].y, &tree_prop[i].h, &tree_prop[i].d, &tree_prop[i].c, &tree_prop[i].p);
-		tree_prop[i].value = tree_prop[i].p * tree_prop[i].h * tree_prop[i].d;
-		tree_prop[i].weight = tree_prop[i].c * tree_prop[i].d * tree_prop[i].h;
-		tree_prop[i].price = tree_prop[i].value;
-	}
 
+        tree_prop = (struct tree_prop*)malloc(sizeof(struct tree_prop));
+		scanf("%d %d %d %d %d %d",&tree_prop->x, &tree_prop->y, &tree_prop->h, &tree_prop->d, &tree_prop->c, &tree_prop->p);
+		tree_prop->value = tree_prop->p * tree_prop->h * tree_prop->d;
+		tree_prop->weight = tree_prop->c * tree_prop->d * tree_prop->h;
+		tree_prop->price = tree_prop->value;
+        insert(tree_prop);
+	}
+    display();
+
+    printf("********\n");
     int time = 0;
     while (time < time_limit)
     {
     	int min, total_price = 0, position;
-    	min = tree_prop[0].x + tree_prop[0].y;
+    	min = head->x + head->y;
 
     	//To find the nearest tree
     	for(i=0;i<no_of_trees;i++)
         {
-            if(tree_prop[i].x + tree_prop[i].y<min)
+            if(tree_prop->x + tree_prop->y < min)
             {
-                min = tree_prop[i].x + tree_prop[i].y;
+                min = tree_prop->x + tree_prop->y;
                 position = i;
+                tree_prop = tree_prop -> next;
             }
         }
 
         //Checking whether the time limit given is sufficient to go to that tree and cut that tree
-        if(tree_prop[position].x + tree_prop[position].y + tree_prop[position].d < time_limit)
+        if(tree_prop->x + tree_prop->y + tree_prop->d < time_limit)
         {
-            total_price = total_price + tree_prop[position].price;
+            total_price = total_price + tree_prop->price;
         }
 
     }
