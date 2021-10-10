7f2c277e53e61d188469287a1c6da014455725c5
McQueen <63579517+mcqueen444@users.noreply.github.com>
2021-10-10 23:24:09 +0530

    Update test.py

--- test.py
+++ test.py
@@ -43,15 +43,16 @@ def upProfit(near_tree):
     profit = 0
     uptrees = sorted([i for i in trees if near_tree["x"] == i["x"] and near_tree["y"] < i["y"]],key=lambda j: j["y"])
     if len(uptrees) != 0:
-        #print("i")
         if near_tree["h"] > abs(uptrees[0]["position"] - near_tree["position"]) and near_tree["weight"] > uptrees[0]["weight"]:
             profit += uptrees[0]["value"]
             temp += [uptrees[0]]
             profit1 = upProfit(uptrees[0])
             profit += profit1[1]
             return temp,profit
-        else:return 0,0
-    else: return 0,0
+        else:
+            return 0,0
+    else:
+        return 0,0
 
 #downProfit function
 def downProfit(near_tree):
@@ -65,11 +66,13 @@ def downProfit(near_tree):
             profit1 = downProfit(downtrees[0])
             profit += profit1[1]
             return temp,profit
-        else:return 0,0
-    else: return 0,0
+        else:
+            return 0,0
+    else:
+        return 0,0
 
 
-#rightProfit function
+# rightProfit function
 def rightProfit(near_tree):
     temp = []
     profit = 0
@@ -81,8 +84,10 @@ def rightProfit(near_tree):
             profit1 = rightProfit(righttrees[0])
             profit += profit1[1]
             return temp,profit
-        else:return 0,0
-    else: return 0,0
+        else:
+            return 0,0
+    else:
+        return 0,0
 
 # leftProfit function
 def leftProfit(near_tree):
@@ -96,57 +101,71 @@ def leftProfit(near_tree):
             profit1 = leftProfit(lefttrees[0])
             profit += profit1[1]
             return temp,profit
-        else: return 0,0
-    else: return 0,0
+        else:
+            return 0,0
+    else:
+        return 0,0
 
 #-----------------------------------------------------------------------------------------------------------------
 
 # taking input of properties for each tree in a list of dicts
 for i in range(no_of_trees):
     x, y, h, d, c, p = map(int,input().split())
-    trees += [{"position":x+y,"x":x,"y":y,"h":h,"d":d,"c":c,"p":p, "value":p*h*d, "weight":c*d*h}]
+    trees += [{"position":x+y,"x":x,"y":y,"h":h,"d":d,"c":c,"p":p, "value":p*h*d, "weight":c*d*h, "full_profit":0}]
 
 # sorting based on position of tree from origin
-trees.sort(key=lambda x:x["position"])
+
+for i in trees:
+    full_profit = cal_profit(i)
+    i["full_profit"] = full_profit[2]
+
 
 time = 0
 total_price = 0
 current_x = current_y = 0
 t = time_limit
+
 # moving
 while time < time_limit and len(trees)>0:
+    trees.sort(key=lambda x:((abs(x["x"]-current_x) + abs(x["y"]-current_y)),-(x["full_profit"]+x["value"]),-x["value"]))
     direction,a,final_profit = cal_profit(trees[0])
 
     if time  < time_limit:
         total_price += trees[0]["value"] + final_profit
         time += trees[0]["d"]
 
-        if current_x < trees[0]["x"] and t >= 0:
+        if current_x < trees[0]["x"] and min(t,trees[0]["x"]-current_x) >= 0:
             print("move right\n"*min(t,trees[0]["x"]-current_x),end="")
             t -= trees[0]["x"]-current_x
-        elif current_x > trees[0]["x"] and t >= 0:
+
+        elif current_x > trees[0]["x"] and min(t,current_x-trees[0]["x"]) >= 0:
             print("move left\n"* min(t,current_x-trees[0]["x"]),end="")
             t -= current_x-trees[0]["x"]
-        if current_y < trees[0]["y"] and t >= 0:
+
+        if current_y < trees[0]["y"] and min(t,trees[0]["y"]-current_y ) >= 0:
             print("move up\n"* min(t,trees[0]["y"]-current_y ),end="")
             t -= trees[0]["y"]-current_y
-        elif current_y > trees[0]["y"] and t >= 0:
+
+        elif current_y > trees[0]["y"] and min(t,current_y-trees[0]["y"]) >= 0:
             print("move down\n"* min(t,current_y-trees[0]["y"]),end="")
             t -= current_y-trees[0]["y"]
 #-----------------
-        if direction == 0 and t >= 0:
+        if direction == 0 and (t - trees[0]["d"]) >= 0 :
             print("cut up")
             t -= trees[0]["d"]
-        elif abs(direction) == 1 and t >= 0:
+
+        elif abs(direction) == 1 and (t - trees[0]["d"]) >= 0:
             print("cut down")
             t -= trees[0]["d"]
-        elif direction == 2 and t >= 0:
+
+        elif direction == 2 and (t - trees[0]["d"]) >= 0:
             print("cut right")
             t -= trees[0]["d"]
-        elif direction == 3 and t >= 0:
+
+        elif direction == 3 and (t - trees[0]["d"]) >= 0:
             print("cut left")
             t -= trees[0]["d"]
-            
+
         time += trees[0]["position"] - current_x - current_y
 
         current_x = trees[0]["x"]
@@ -156,5 +175,5 @@ while time < time_limit and len(trees)>0:
 
 
     if a != 0 :
-        for j in a:
-            trees.remove(j)
+        for i in a:
+            trees.remove(i)
